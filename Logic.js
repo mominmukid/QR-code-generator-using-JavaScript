36727ac753f45a5b78773457b29026ef128ef71a
mominmukid <118976436+mominmukid@users.noreply.github.com>
2024-02-06 23:15:59 +0530

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -1,6 +1,6 @@
 
 const submit=document.getElementById('submit');
-const con=document.querySelector('#url').value;
+const con=document.querySelector('#url');
 const imgBox=document.getElementById('qr');
 const img=document.getElementById('qr-img');
 
@@ -8,7 +8,7 @@ const GenrateQRcode=()=>{
     if(con===''){
         alert("Enter valid Text or Link");
     }else{
-        img.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${con}`  
+        img.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${con.value}`  
     }
 }
 submit.addEventListener('click',()=>{
