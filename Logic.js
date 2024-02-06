dee629d9c9f4727993778efdf38b37e5f33484f7
Mukid Momin <mominmukid@gmail.com>
2024-02-06 20:39:07 +0530

    COmple the proect and then we can genrate the QR code

---
+++ Logic.js
@@ -0,0 +1,19 @@
+const submit=document.getElementById('submit');
+const con=document.querySelector('#url').value;
+const imgBox=document.getElementById('qr');
+const img=document.getElementById('qr-img');
+
+const GenrateQRcode=()=>{
+    if(con===''){
+        alert("Enter valid Text or Link");
+    }else{
+        img.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${con}`  
+    }
+}
+submit.addEventListener('click',()=>{
+      GenrateQRcode();
+});
+
+const getdata=()=>{
+    localStorage.getItem('data',img);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QR-code-generator-using-JavaScript
+
+this the project to create the QR code and any can scan this qr code and get information
